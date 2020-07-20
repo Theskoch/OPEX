bb5e0a9b6ddca86eec278d936dbb8a053d30c129
TheSkoCh <geimer.repgas@mail.ru>
2020-07-21 00:47:38 +0300

    v_2

--- front_and/main page/main_page.js
+++ front_and/main page/main_page.js
@@ -1,40 +1,59 @@
 
 
-let triger=1;
+
+
+// left_panel______________________________________________________________________________________________
 
 let mass_id = ["track_buy_id_elem","work_sklad","bibl","otch"];
 let mass_text = ["Трекер закупок","Работа со складом","Справочники","Отчеты"];
 
-function sleep(milliseconds) {
-    const date = Date.now();
-    let currentDate = null;
-    do {
-      currentDate = Date.now();
-    } while (currentDate - date < milliseconds);
+
+function sleep_left_panel(restart) {
+  
+  if (localStorage.triger == undefined)
+  {
+    localStorage.setItem('triger', 1);
   }
 
-
-function sleep_left_panel() {
-    if (triger==1){
-
-        document.getElementById("left_panel").style.width="60px";
-
-        for(let i=0;i<4;i++)
-        {
-            document.getElementById(mass_id[i]).innerHTML="";
-        }
-
-        triger=triger-1;
+  if (restart)
+  {
+    if(localStorage.triger==1)
+    {
+      localStorage.triger = 0;
     }
+    else
+    {
+      localStorage.triger = 1;
+    }
+  }
+
+  if (localStorage.triger=="1"){
+
+      document.getElementById("left_panel").style.width="60px";
+
+      for(let i=0;i<4;i++)
+      {
+        document.getElementById(mass_id[i]).innerHTML="";
+      }
+      localStorage.triger = 0;
+  }
 
     else{
+
         document.getElementById("left_panel").style.width="300px";
             
         for(let i=0;i<4;i++)
         {
-            document.getElementById(mass_id[i]).innerHTML=mass_text[i];
+          document.getElementById(mass_id[i]).innerHTML=mass_text[i];
         }
 
-        triger=triger+1;
+        localStorage.triger = 1;
     }
   }
+
+
+document.addEventListener("DOMContentLoaded", function() {sleep_left_panel(true);});
+
+
+
+// left_panel______________________________________________________________________________________________
