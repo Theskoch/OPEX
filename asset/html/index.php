639694f21d4cd30581368ddc1a6afb89b6f29e95
TheSkoCh <geimer.repgas@mail.ru>
2020-07-30 00:45:35 +0300

    new branch prod

--- asset/html/index.php
+++ asset/html/index.php
@@ -9,6 +9,7 @@
     <header class="header">
     <h1>shapka_holodec</h1>
 </header>
+    русский где ????
     <h4><a href="./input.php">input</a></h4>
     <h4><a href="./register.php">register</a></h4>
     <h4><a href="./main_page.php">main_page</a></h4>
